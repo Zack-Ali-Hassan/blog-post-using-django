c70c925ec25483879b71cb24cae793c7592d3dbe
Zack-Ali-Hassan <yares6667@gmail.com>
2024-08-23 16:38:28 +0300

    aug 23, 2024 third commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import PostListView
 from . import views
 urlpatterns = [
-    path('', views.Home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.About, name='blog-about'),
 ]
